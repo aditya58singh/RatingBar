c3b5cafb13332c438cd199ca9e7baf54a772c298
aditya <aditya58singh@gmail.com>
2019-03-07 22:52:40 +0530

    1 commit

---
+++ app/src/main/java/androidhire/com/ratingbar/MainActivity.java
@@ -0,0 +1,32 @@
+package androidhire.com.ratingbar;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.RatingBar;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    RatingBar ratingBar;
+    Button button;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        ratingBar = findViewById(R.id.ratingBar);
+        button = findViewById(R.id.button);
+
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                float ratingcount = ratingBar.getRating();
+
+                Toast.makeText(getApplicationContext(), "Rating is :" + ratingcount, Toast.LENGTH_LONG).show();
+            }
+        });
+    }
+}
